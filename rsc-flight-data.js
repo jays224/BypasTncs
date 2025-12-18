ab478341566f2be6b24135c811b5f37d499da9b1
jays224 <kentdomnic@gmail.com>
2025-12-18 17:01:07 +0800

    Create rsc-flight-data.js

---
+++ rsc-flight-data.js
@@ -0,0 +1,9 @@
+1:"$Sreact.fragment"
+2:I[47257,["/_next/static/chunks/ff1a16fafef87110.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/247eb132b7f7b574.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS"],"ClientPageRoot"]
+3:I[87636,["/_next/static/chunks/1d89ed972a208c42.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/f44cbbcd27d8496a.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/62fe7e5a1093aa3f.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS"],"default"]
+6:I[97367,["/_next/static/chunks/ff1a16fafef87110.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/247eb132b7f7b574.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS"],"OutletBoundary"]
+7:"$Sreact.suspense"
+0:{"buildId":"uvw8dgetMcrU9_j4-HF1V","rsc":["$","$1","c",{"children":[["$","$L2",null,{"Component":"$3","serverProvidedParams":{"searchParams":{},"params":{},"promises":["$@4","$@5"]}}],[["$","script","script-0",{"src":"/_next/static/chunks/f44cbbcd27d8496a.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","async":true}],["$","script","script-1",{"src":"/_next/static/chunks/62fe7e5a1093aa3f.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","async":true}]],["$","$L6",null,{"children":["$","$7",null,{"name":"Next.MetadataOutlet","children":"$@8"}]}]]}],"loading":null,"isPartial":false}
+4:{}
+5:"$0:rsc:props:children:0:props:serverProvidedParams:params"
+8:null
