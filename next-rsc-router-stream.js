fd694d4ce48e8bdd087ee315515afbacdb90c3cc
jays224 <kentdomnic@gmail.com>
2025-12-18 17:01:33 +0800

    Create next-rsc-router-stream.js

---
+++ next-rsc-router-stream.js
@@ -0,0 +1,4 @@
+1:"$Sreact.fragment"
+2:I[39756,["/_next/static/chunks/ff1a16fafef87110.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/247eb132b7f7b574.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS"],"default"]
+3:I[37457,["/_next/static/chunks/ff1a16fafef87110.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS","/_next/static/chunks/247eb132b7f7b574.js?dpl=dpl_Ht97dG1uFHXh48fvQM5GQzV3ksrS"],"default"]
+0:{"buildId":"uvw8dgetMcrU9_j4-HF1V","rsc":["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","template":["$","$L3",null,{}]}]]}],"loading":null,"isPartial":false}
